2219db3086a3ab4856876ee27fa9883e76992e64
Hilde Cabrera <HJCA3120@gmail.com>
2024-11-12 10:03:53 -0400

    Commit Inicial

---
+++ js/products.js
@@ -0,0 +1,136 @@
+const products = {
+    1: {
+        name: "Letter Bracelet",
+        price: "$100",
+        details: "<b>-Tamaño de las Letras:</b> EAproximadamente 5 mm de altura, personalizables según el diseño.<br> <b>-Ancho de la Pulsera:</b>  3 mm, ofreciendo un perfil elegante y discreto.<br> <b>-Longitud:</b> Disponible en varias longitudes (generalmente de 6.5 a 7.5 pulgadas) para un ajuste perfecto.",
+        description: "La pulsera de letras en oro de 14k es una joya única y personal que te permite llevar tus iniciales o una palabra significativa cerca de ti. Con un diseño delicado y moderno, esta pulsera es perfecta para cualquier ocasión y se puede combinar fácilmente con otros accesorios.",
+        materials: "<b>Oro de 14k</b><br>Cuando se cuida adecuadamente, nuestras piezas de oro de 14k pueden durar para siempre. Con su hermoso y sutil tono, puedes usar tu joyería todos los días, en cualquier lugar.",
+        images: [
+            "/img/bracelets/0-14KLETTERS-14kLetterBraceletA-14K-TopDown_024.webp",
+            "/img/bracelets/bracelets_hover_img/0_MiniPearlSatelliteBracelet_V_Hero.webp"
+        ]
+    },
+
+    2: {
+        name: "Diamond Tennis Bracelet 1.8mm",
+        price: "$170",
+        details: "<b>-Peso Promedio de los Diamantes:</b> Entre 1.83 ct y 2.3 ct, dependiendo de la longitud de la pulsera.<br> <b>-Tamaño de las Piedras:</b> 1.8 mm de diámetro por cada diamante. <br> <b>-Ancho de la Pulsera:</b> 2 mm, ofreciendo un perfil delgado y delicado. <br> <b>-Altura de la Pulsera:</b> 2.4 mm, asegurando una apariencia elegante en la muñeca.",
+        description: "Eleva tu estilo con nuestra deslumbrante pulsera tennis de diamantes de 1.8 mm. Este exquisito accesorio está meticulosamente diseñado con una línea continua de diamantes brillantes, que reflejan la luz de manera deslumbrante en cada movimiento.",
+        materials: "<b>Oro de 14k</b><br>Cuando se cuida adecuadamente, nuestras piezas de oro de 14k pueden durar para siempre. Con su hermoso y sutil tono, puedes usar tu joyería todos los días, en cualquier lugar. <br><br> <b>Diamantes Naturales</b> <br> Nuestros diamantes extraídos son obtenidos de manera responsable de proveedores que siguen prácticas libres de conflicto y socialmente responsables. ",
+        images: [
+            "/img/bracelets/0-BoldDiamondTennisBracelet-WG-Front_183.webp",
+            "/img/bracelets/bracelets_hover_img/1-BoldDiamondTennisBraceletWhiteGold-14K-Stack1_028.webp"
+        ]
+    },
+
+    3: {
+        name: "Boyfriend Bold Pavé Diamond Bar Bracelet",
+        price: "$600",
+        details: "<b>-Peso Promedio de los Diamantes:</b> Varía entre 0.5 ct y 1 ct, dependiendo del tamaño y la longitud de la barra.<br> <b>-Estilo Pavé:</b> Los diamantes están colocados en un engaste pavé, lo que crea una superficie brillante continua a lo largo de la barra. <br> <b>-Ancho de la Barra:</b> 5 mm, lo que le otorga una apariencia audaz y de impacto. <br> <b>-Longitud de la Pulsera:</b> Disponible en 6.5 a 7.5 pulgadas, con un ajuste cómodo para diferentes tamaños de muñeca.",
+        description: "La Boyfriend Bold Pavé Diamond Bar Bracelet es una pieza audaz y moderna que combina elegancia y estilo contemporáneo. Con su barra pavé de diamantes y un diseño robusto, esta pulsera destaca por su presencia llamativa y su brillo deslumbrante, perfecta para quienes buscan una joya que haga una declaración.",
+        materials: "<b>Oro de 14k</b><br>Cuando se cuida adecuadamente, nuestras piezas de oro de 14k pueden durar para siempre. Con su hermoso y sutil tono, puedes usar tu joyería todos los días, en cualquier lugar. <br><br> <b>Diamantes Naturales</b> <br> Nuestros diamantes extraídos son obtenidos de manera responsable de proveedores que siguen prácticas libres de conflicto y socialmente responsables. ",
+        images: [
+            "/img/bracelets/0-BoyfriendBoldPaveBarBracelet-14K-TopDown_437.webp",
+            "/img/bracelets/bracelets_hover_img/1-BoyfriendBoldPaveBarBracelet-14k-Stack_025.webp"
+        ]
+    },
+
+    4: {
+        name: "Diamond Half Tennis Bracelet",
+        price: "$980",
+        details: "<b>-Peso Promedio de los Diamantes:</b> Entre 0.5 ct y 1 ct, dependiendo de la longitud y el tamaño de los diamantes. <br> <b>-Tamaño de las Piedras:</b>Cada diamante tiene aproximadamente 2 mm de diámetro, creando un brillo delicado pero notable.<br><b>Longitud de la Pulsera:</b>Disponible en 6.5 a 7.5 pulgadas, personalizable para un ajuste cómodo.<br> <b>-Ancho de la Pulsera:</b> 2 mm, lo que le da un perfil elegante y ligero.",
+        description: "La Diamond Half Tennis Bracelet es una versión moderna y elegante del clásico diseño de pulsera tennis. Esta pieza destaca por tener solo la mitad de la pulsera engastada con diamantes, creando un contraste sutil y refinado. Ideal para quienes buscan una joya sofisticada con un toque de originalidad.",
+        materials: "<b>Oro de 14k</b><br>Cuando se cuida adecuadamente, nuestras piezas de oro de 14k pueden durar para siempre. Con su hermoso y sutil tono, puedes usar tu joyería todos los días, en cualquier lugar. <br><br> <b>Diamantes Naturales</b> <br>Nuestros diamantes extraídos son obtenidos de manera responsable de proveedores que siguen prácticas libres de conflicto y socialmente responsables.",
+        images: [
+
+            "/img/bracelets/0-DiamondHalfTennisBracelet-14K-Front_233.webp",
+
+            "/img/bracelets/bracelets_hover_img/1-DiamondHalfTennisBracelet-14K-Stack_029.webp"
+        ]
+    },
+
+    5: {
+        name: "Diamond Half Tennis Bracelet",
+        price: "$138",
+        details: "<b>Peso Promedio de los Diamantes:</b> 0.5 ct - 1 ct <br> <b>Tamaño de las Piedras:</b> 2 mm de diámetro <br> <b>Ancho de la Pulsera:</b> 2 mm <br> <b>Longitud:</b> 6.5 a 7.5 pulgadas",
+        description: "La Diamond Half Tennis Bracelet es una versión moderna del clásico diseño de pulsera tennis, con diamantes engastados solo en la mitad, lo que le da un estilo único y sofisticado.",
+        materials: "<b>Oro de 14k:</b> Cuando se cuida adecuadamente, nuestras piezas de oro de 14k pueden durar para siempre. Con su hermoso y sutil tono, puedes usar tu joyería todos los días, en cualquier lugar.<br><br><br> <b>Diamantes Naturales:</b> Nuestros diamantes extraídos son obtenidos de manera responsable de proveedores que siguen prácticas libres de conflicto y socialmente responsables.",
+        images: [
+            "/img/bracelets/0-SpheresBracelet-14K-TopDown_169.webp",
+            "/img/bracelets/bracelets_hover_img/1-SpheresBracelet-YG-OnFigure-Stack-030.webp"
+        ]
+    },
+
+    6: {
+        name: "Heart Station Bracelet",
+        price: "$100",
+        details: "<b>Longitud de la Pulsera:</b> 6.5 a 7.5 pulgadas <br> <b>Corazones:</b> Corazones delicados distribuidos uniformemente <br> <b>Ancho de la Pulsera:</b> 2 mm <br> <b>Cierre:</b> Cierre de seguridad tipo langosta",
+        description: "La Heart Station Bracelet es una pieza romántica y delicada que presenta pequeños corazones distribuidos a lo largo de la pulsera. Con un diseño sencillo pero lleno de encanto, es perfecta para el uso diario o como un regalo especial.",
+        materials: "<b>Oro de 14k:</b> Cuando se cuida adecuadamente, nuestras piezas de oro de 14k pueden durar para siempre. Con su hermoso y sutil tono, puedes usar tu joyería todos los días, en cualquier lugar.<br><br><br> <b>Corazones de Oro:</b> Los corazones están finamente trabajados en oro de 14k, ofreciendo una apariencia delicada y romántica.",
+        images: [
+            "/img/bracelets/0-VERMEILBRACELETS_MultiStationBraceletHeartVermeil_V_TopDown_033.webp",
+            "/img/bracelets/bracelets_hover_img/1-VERMEILBRACELETS_MultiStationBraceletHeartVermeil_V_Stack_019.webp"
+        ]
+    },
+
+    7: {
+        name: "Anchor Chain Bracelet",
+        price: "$178",
+        details: "<b>Longitud de la Pulsera:</b> 6.5 a 7.5 pulgadas <br> <b>Diseño de Cadena:</b> Estilo ancla tradicional <br> <b>Ancho de la Pulsera:</b> 3 mm <br> <b>Cierre:</b> Cierre de seguridad tipo langosta",
+        description: "La Anchor Chain Bracelet es una pulsera robusta y elegante, inspirada en el clásico diseño de cadena de ancla. Su estilo atemporal y durabilidad hacen de esta pieza un accesorio versátil para el día a día o para ocasiones especiales.",
+        materials: "<b>Oro de 14k:</b> Cuando se cuida adecuadamente, nuestras piezas de oro de 14k pueden durar para siempre. Con su hermoso y sutil tono, puedes usar tu joyería todos los días, en cualquier lugar.<br><br><br> <b>Diseño de Cadena:</b> El diseño de cadena de ancla es conocido por su resistencia y sofisticación, ofreciendo un equilibrio perfecto entre estilo y funcionalidad.",
+        images: [
+            "/img/bracelets/0_AnchorChain_bracelet_yg_topdown_hero.webp",
+            "/img/bracelets/bracelets_hover_img/1_AnchorChain_bracelet_yg_solo.webp"
+        ]
+    },
+
+    8: {
+        name: "Mini Pearl Satellite Bracelet",
+        price: "$100",
+        details: "<b>Longitud de la Pulsera:</b> 6.5 a 7.5 pulgadas <br> <b>Perlas:</b> Pequeñas perlas distribuidas a lo largo de la cadena <br> <b>Ancho de la Pulsera:</b> 1.5 mm <br> <b>Cierre:</b> Cierre de seguridad tipo langosta",
+        description: "La Mini Pearl Satellite Bracelet es una pulsera delicada y femenina que presenta pequeñas perlas finamente distribuidas a lo largo de una cadena sutil. Perfecta para quienes buscan un toque elegante y sofisticado para su look diario o para ocasiones especiales.",
+        materials: "<b>Oro de 14k:</b> Cuando se cuida adecuadamente, nuestras piezas de oro de 14k pueden durar para siempre. Con su hermoso y sutil tono, puedes usar tu joyería todos los días, en cualquier lugar.<br><br><br> <b>Perlas Naturales:</b> Las perlas son cultivadas cuidadosamente para asegurar su calidad, brindando un toque suave y femenino a esta pieza.",
+        images: [
+            "/img/bracelets/0_MiniPearlSatelliteBracelet_V_Hero.webp",
+            "/img/bracelets/bracelets_hover_img/1_MiniPearlSatelliteBracelet_V_Stack_0830.webp"
+        ]
+    }
+
+};
+
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+const productId = getQueryParam('id');
+
+if (products[productId]) {
+    // Obtener los elementos HTML donde se mostrará la información
+    const productName = document.getElementById("product_name");
+    const productPrice = document.getElementById("price");
+    const productDetails = document.getElementById("details");
+    const productDescription = document.getElementById("description");
+    const productMaterials = document.getElementById("materials");
+    const productImages = document.querySelectorAll("#product_img"); // Obtener todas las imágenes
+
+    // Actualizar el contenido con los datos del producto seleccionado
+    productName.textContent = products[productId].name;
+    productPrice.textContent = products[productId].price;
+    productDetails.innerHTML = products[productId].details; // Usar innerHTML para permitir HTML
+    productDescription.innerHTML = products[productId].description;
+    productMaterials.innerHTML = products[productId].materials;
+
+    // Actualizar las imágenes
+    products[productId].images.forEach((imgSrc, index) => {
+        if (productImages[index]) {
+            productImages[index].src = imgSrc; // Cambiar el src de cada imagen
+            productImages[index].alt = `${products[productId].name} imagen ${index + 1}`; // Alternativa para accesibilidad
+        }
+    });
+} else {
+    // Si no existe el producto, mostrar un mensaje de error
+    const productName = document.getElementById("product_name");
+    productName.textContent = "Producto no encontrado";
+}
